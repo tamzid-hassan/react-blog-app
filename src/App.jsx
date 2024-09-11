c521e10c1b6fb280d8854fe0474c39d89800f754
tamzid.hassan <iamtamzid96+github@gmail.com>
2024-09-12 00:08:19 +0600

    Appwrite Services setup for Authetication

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css"
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
-    <h1>Appwrite Blog App</h1>
+      <h1 className="w-1/2 bg-orange-700">Appwrite Blog App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
